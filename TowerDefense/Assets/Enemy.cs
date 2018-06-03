13083a522fb71b463ffe9332940d8ff0e52faa1f
dorog <dorogi.x@gmail.com>
2018-06-03 23:55:51 +0200

    AI, first turret
    Spawn first version is ready. Turret can follow the nearest enemy in its range.

---
+++ TowerDefense/Assets/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+        target = WayPoints.points[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized*speed*Time.deltaTime);
+        if(Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextWayPoint();
+        }
+	}
+
+    void GetNextWayPoint()
+    {
+        if(wavepointIndex >= WayPoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        wavepointIndex++;
+        target = WayPoints.points[wavepointIndex];
+    }
+}
